ba8a0cc1bcdddfeba429a1e9503436e0d16450a8
wade-ary <161520052+wade-ary@users.noreply.github.com>
2024-12-23 12:57:23 +0530

    redo

--- osmdata.py
+++ osmdata.py
@@ -1,36 +1,10 @@
 import osmnx as ox
-import matplotlib.pyplot as plt
 
-# Optional: Configure OSMnx settings
-ox.config(use_cache=True, log_console=True)
+# Define the area of interest
+place = "Boston, Massachusetts, USA"
 
-place_name = "Boston, Massachusetts, USA"
+# Define the network type to include walkways
+G = ox.graph_from_place(place, network_type='walk', simplify=True)
 
-G_walk = ox.graph_from_place(place_name, network_type="walk")
-
-# Define tags for parks and recreational areas
-tags = {'leisure': ['park', 'garden', 'golf_course', 'playground', 'stadium']}
-
-# Download polygons for parks in Boston
-parks = ox.geometries_from_place(place_name, tags=tags)
-
-# Optional: Save parks data to a file
-parks.to_file("boston_parks.geojson", driver='GeoJSON')
-
-trail_tags = {'highway': ['path', 'footway', 'cycleway']}
-
-# Download trails and paths in Boston
-trails = ox.geometries_from_place(place_name, tags=trail_tags)
-
-# Optional: Save trails data to a file
-trails.to_file("boston_trails.geojson", driver='GeoJSON')
-
-fig, ax = ox.plot_graph(G_walk, show=False, close=False, node_size=0, edge_color='gray')
-
-# Plot parks on the same map
-parks.plot(ax=ax, facecolor='green', edgecolor='k', alpha=0.5)
-
-# Optional: Plot trails
-trails.plot(ax=ax, facecolor='blue', edgecolor='k', alpha=0.3)
-
-plt.show()
+# Save the graph for later use
+ox.save_graphml(G, "boston_walkways.graphml")
